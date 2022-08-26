0005afc0b4220f830dc0ba134d2ca7b8cfbab871
Paloma Tejeda <palomatejeda81@gmail.com>
2022-08-26 16:39:49 +0200

    first commit

---
+++ src/shaders/water/fragment.glsl
@@ -0,0 +1,11 @@
+uniform vec3 uDepthColor;
+uniform vec3 uSurfaceColor;
+
+varying float vElevation;
+
+void main()
+{
+    vec3 color = mix(uDepthColor, uSurfaceColor, vElevation * 6.0);
+
+    gl_FragColor = vec4(color, 1.0);
+}
